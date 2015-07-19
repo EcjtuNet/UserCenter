6211444ec6f88a8e080e724460dbfc6c20c91ead
wtbhk <wtbhk@Live.cn>
2015-07-19 22:57:31 +0800

    注释

--- cronwork.py
+++ cronwork.py
@@ -14,7 +14,7 @@ import json
 @db_session
 def cronwork1():
     url = Config.get('StudentInfo_update_url')
-    s = urllib.urlopen(url).read()[3:]
+    s = urllib.urlopen(url).read()[3:] #Windows下返回的json需要去掉前三位
     a = json.loads(s)
     for row in a:
         s = StudentInfo(StudentID=str(row['StudentID']))
